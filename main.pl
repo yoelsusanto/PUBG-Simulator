6b6574e238d690181b515af02658220f908b1a2a
yoelpro <yoelsusantopro@yahoo.com>
2018-11-19 14:48:58 +0700

    Add start and help

--- main.pl
+++ main.pl
@@ -1,11 +1,11 @@
 /* fakta */
-dynamic(inventory/1).
-dynamic(health/1).
-dynamic(armor/1).
-dynamic(currweapon/1).
-dynamic(position/2).    % position predicate
-dynamic(player/1).      % player predicate
-dynamic(play/1).        % is playing predicate
+:- dynamic(inventory/1).
+:- dynamic(health/1).
+:- dynamic(armor/1).
+:- dynamic(currweapon/1).
+:- dynamic(position/2).    % position predicate
+:- dynamic(player/1).      % player predicate
+:- dynamic(play/1).        % is playing predicate
 
 inventory(none).        % set inventory to no item
 
@@ -30,6 +30,7 @@ location(1, 7, 5, 15, 'labtek V').
 location(6, 7, 12, 15, 'labtek VI').
 location(13, 7, 15, 15, 'CC barat').
 
+% set play to default false
 play(false).
 
 mapM(
@@ -56,61 +57,62 @@ mapM(
 /* rule */
 start :-
     % randomly place player
-    random(1, 16, X), random(1, 16, Y), assert(player([X, Y])),
+    random(1, 16, X), random(1, 16, Y), asserta(player([X, Y])),
     % randomly place weapon
     forall(
-        (random(2, 5, N), between(1, N, _)), forall(weapon(Z), (random(1, 16, A), random(1, 16, B), assert(position(Z, [A, B]))))
+        (random(2, 5, N), between(1, N, _)), forall(weapon(Z), (random(1, 16, A), random(1, 16, B), asserta(position(Z, [A, B]))))
         ),
     % erase play from false to true
     retract(play(false)),
-    assert(play(true)),
-    writeln(',d88~~\\                         d8                 d8              e                 d8           ,88~-_      88~\\    88~\\   888   ,e,                        '),
-    writeln('8888       /~~~8e   888-~88e  _d88__   e88~~8e   _d88__           d8b      888-~\\  _d88__        d888   \\   _888__  _888__   888    "    888-~88e    e88~~8e  '),
-    writeln('`Y88b          88b  888  888   888    d888  88b   888            /Y88b     888      888         88888    |   888     888     888   888   888  888   d888  88b '),
-    writeln(' `Y88b,   e88~-888  888  888   888    8888__888   888           /  Y88b    888      888         88888    |   888     888     888   888   888  888   8888__888 '),
-    writeln('   8888  C888  888  888  888   888    Y888    ,   888          /____Y88b   888      888          Y888   /    888     888     888   888   888  888   Y888    , '),
-    writeln('\\__88P''   "88_-888  888  888   "88_/   "88___/    "88_/       /      Y88b  888      "88_/         `88_-~     888     888     888   888   888  888    "88___/  '), nl, nl,
-    writeln('Welcome to the battlefield!'),
-    writeln('You have been chosen as one of the lucky contestants. Be the last man standing and you will be remembered as one of the victors.'), nl,
-    writeln('Available commands:'),
-    writeln('start. -- start the game!'),
-    writeln('help. -- show available commands'),
-    writeln('quit. -- quit the game'),
-    writeln('look. -- look around you'),
-    writeln('n. s. e. w. -- move'),
-    writeln('map. -- look at the map and detect enemies'),
-    writeln('take(Object). -- pick up an object'),
-    writeln('drop(Object). -- drop an object'),
-    writeln('use(Object). -- use an object'),
-    writeln('attack. -- attack enemy that crosses your path'),
-    writeln('status. -- show your status'),
-    writeln('save(Filename). -- save your game'),
-    writeln('load(Filename). -- load previously saved game'),
-    writeln('Legends:'),
-    writeln('W = weapon'),
-    writeln('A = armor'),
-    writeln('M = medicine'),
-    writeln('O = ammo'),
-    writeln('P = player'),
-    writeln('E = enemy'),
-    writeln('- = accessible'),
-    writeln('X = inaccessible').
+    asserta(play(true)),
+    write(',d88~~\\                         d8                 d8              e                 d8           ,88~-_      88~\\    88~\\   888   ,e,                        '), nl,
+    write('8888       /~~~8e   888-~88e  _d88__   e88~~8e   _d88__           d8b      888-~\\  _d88__        d888   \\   _888__  _888__   888    "    888-~88e    e88~~8e  '), nl,
+    write('`Y88b          88b  888  888   888    d888  88b   888            /Y88b     888      888         88888    |   888     888     888   888   888  888   d888  88b '), nl,
+    write(' `Y88b,   e88~-888  888  888   888    8888__888   888           /  Y88b    888      888         88888    |   888     888     888   888   888  888   8888__888 '), nl,
+    write('   8888  C888  888  888  888   888    Y888    ,   888          /____Y88b   888      888          Y888   /    888     888     888   888   888  888   Y888    , '), nl,
+    write('\\__88P''   "88_-888  888  888   "88_/   "88___/    "88_/       /      Y88b  888      "88_/         `88_-~     888     888     888   888   888  888    "88___/  '), nl, nl,
+    write('Welcome to the battlefield!'), nl,
+    write('You have been chosen as one of the lucky contestants. Be the last man standing and you will be remembered as one of the victors.'), nl, nl,
+    write('Available commands:'), nl,
+    write('   start. -- start the game!'), nl,
+    write('   help. -- show available commands'), nl,
+    write('   quit. -- quit the game'), nl,
+    write('   look. -- look around you'), nl,
+    write('   n. s. e. w. -- move'), nl,
+    write('   map. -- look at the map and detect enemies'), nl,
+    write('   take(Object). -- pick up an object'), nl,
+    write('   drop(Object). -- drop an object'), nl,
+    write('   use(Object). -- use an object'), nl,
+    write('   attack. -- attack enemy that crosses your path'), nl,
+    write('   status. -- show your status'), nl,
+    write('   save(Filename). -- save your game'), nl,
+    write('   load(Filename). -- load previously saved game'), nl, nl,
+    write('Legends:'), nl,
+    write('   W = weapon'), nl,
+    write('   A = armor'), nl,
+    write('   M = medicine'), nl,
+    write('   O = ammo'), nl,
+    write('   P = player'), nl,
+    write('   E = enemy'), nl,
+    write('   - = accessible'), nl,
+    write('   X = inaccessible'), nl,
+    asserta(play(true)).
 
 % if quit, make play to false and retract player position
-quit :- retract(play(true)), assert(play(false)), retractall(player(_)).
+quit :- retract(play(true)), asserta(play(false)), retractall(player(_)).
 
 map :- play(X), X == false, !, write('You must start the game using "start." first.'), fail.
 map :- mapM(M), printMatrix(M).
 
 % move player position
 n :- play(X), X == false, !, write('You must start the game using "start." first.'), fail.
-n :- player([X|Y]), Xn is X + 1, retractall(player(_)), assert(player([Xn|Y])).
+n :- player([X|Y]), Xn is X + 1, retractall(player(_)), asserta(player([Xn|Y])), areaAround.
 s :- play(X), X == false, !, write('You must start the game using "start." first.'), fail.
-s :- player([X|Y]), Xn is X - 1, retractall(player(_)), assert(player([Xn|Y])).
+s :- player([X|Y]), Xn is X - 1, retractall(player(_)), asserta(player([Xn|Y])), areaAround.
 e :- play(X), X == false, !, write('You must start the game using "start." first.'), fail.
-e :- player([X, Y]), Yn is Y + 1, retractall(player(_)), assert(player([X, Yn])).
+e :- player([X, Y]), Yn is Y + 1, retractall(player(_)), asserta(player([X, Yn])), areaAround.
 w :- play(X), X == false, !, write('You must start the game using "start." first.'), fail.
-w :- player([X, Y]), Yn is Y - 1, retractall(player(_)), assert(player([X, Yn])).
+w :- player([X, Y]), Yn is Y - 1, retractall(player(_)), asserta(player([X, Yn])), areaAround.
 
 % Dipanggil pada saat ada perintah move
 areaAround :-
@@ -158,3 +160,31 @@ status :-
     currweapon(Z), !, write('Weapon : '), write(Z),
     inventory(A), !, write('Inventory : '), write(A), write(' '),
     forall((inventory(B), B \== A, B \== none), (write(A), write(' '))), nl.
+
+% help
+help :- play(X), X == false, !, write('You must start the game using "start." first.'), fail.
+help :-
+    write('Available commands:'), nl,
+    write('   start. -- start the game!'), nl,
+    write('   help. -- show available commands'), nl,
+    write('   quit. -- quit the game'), nl,
+    write('   look. -- look around you'), nl,
+    write('   n. s. e. w. -- move'), nl,
+    write('   map. -- look at the map and detect enemies'), nl,
+    write('   take(Object). -- pick up an object'), nl,
+    write('   drop(Object). -- drop an object'), nl,
+    write('   use(Object). -- use an object'), nl,
+    write('   attack. -- attack enemy that crosses your path'), nl,
+    write('   status. -- show your status'), nl,
+    write('   save(Filename). -- save your game'), nl,
+    write('   load(Filename). -- load previously saved game'), nl, nl,
+    write('Legends:'), nl,
+    write('   W = weapon'), nl,
+    write('   A = armor'), nl,
+    write('   M = medicine'), nl,
+    write('   O = ammo'), nl,
+    write('   P = player'), nl,
+    write('   E = enemy'), nl,
+    write('   - = accessible'), nl,
+    write('   X = inaccessible'), nl.
+    
